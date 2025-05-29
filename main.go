220e38b1e14dbf6f2327c709a6e474192fc6372f
RahnTahn <uprightson@gmail.com>
2025-05-28 17:50:55 -0700

    C: completed reader section loop

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+func main() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+		answer := cleanInput(reader.Text())
+		fmt.Printf("Your command was: %v", answer[0])
+		fmt.Println()
+
+	}
+}
 func cleanInput(text string) []string {
 	cleaned := strings.Fields(strings.ToLower(text))
 	return cleaned
